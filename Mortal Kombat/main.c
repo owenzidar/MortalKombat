edb7c83911f38291784f745e1d16038d72de7c83
Owen Zidar <owenzidar@owens-mbp.dhcp.nd.edu>
2014-02-15 12:08:15 -0500

    Initial Commit

---
+++ Mortal Kombat/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Mortal Kombat
+//
+//  Created by Owen Zidar on 2/15/14.
+//  Copyright (c) 2014 Owen Zidar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
