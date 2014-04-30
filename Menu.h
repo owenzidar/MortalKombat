3617d7f39f07473a1c49964a94fe1effbb8d1400
kepp14 <kepp@nd.edu>
2014-04-29 23:42:25 -0400

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -1,6 +1,7 @@
 /*
-	Class which represents controls and displays the main menu
-	
+Kevin Epp, Mitch Patin, Owen Zidar  CSE 20212
+Final Project  Menu.h
+Header for Menu class, which displays main menu
 */
 
 #ifndef MENU_H
@@ -21,25 +22,25 @@ using namespace std;
 
 class Menu {
 	public:
-		Menu();
-		SDL_Surface * load_image( std::string filename );
-		void apply_surface(int, int, SDL_Surface*, SDL_Surface*, SDL_Rect*);
-		bool init();
-		bool load_files();
-		void clean_up();
-		int* go();
+		Menu(); //Constructor
+		SDL_Surface * load_image( std::string filename ); //Load an image
+		void apply_surface(int, int, SDL_Surface*, SDL_Surface*, SDL_Rect*); //Apply image to a surface
+		bool init(); //Initialize SDL screen
+		bool load_files(); //Load picture files
+		void clean_up(); //Free everything
+		int* go(); //Run the main menu
 
 	private:
-		vector <SDL_Surface *> players;
-		vector <SDL_Surface *> levels;
-		SDL_Surface *controlsScreen;
-		SDL_Surface *menuScreen;
-		SDL_Surface *screen;
+		vector <SDL_Surface *> players; //Vector containing images of all characters
+		vector <SDL_Surface *> levels; //Vector containing images of all levels
+		SDL_Surface *controlsScreen; //Images of the controls screen
+		SDL_Surface *menuScreen; //Image of the menu screen
+		SDL_Surface *screen; //The main SDL screen
 		//The event structure
 		SDL_Event event;
-		int levelNum;
-		int player1Num;
-		int player2Num;
+		int levelNum; //Current level choice
+		int player1Num; //Current character choice for Player 1
+		int player2Num; //Current character choice for Player 2
 };
 
 #endif
